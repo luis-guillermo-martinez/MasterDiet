30b88b8e44538ab2a89afcef51f717139000144b
Luis G. Martinez <99708664+luis-guillermo-martinez@users.noreply.github.com>
2022-09-30 06:28:47 -0300

    Variables_firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB-tYVS6p0Erk1ObW-0MBSo8xloGWAKdk0",
+    authDomain: "masterdiet-897a6.firebaseapp.com",
+    projectId: "masterdiet-897a6",
+    storageBucket: "masterdiet-897a6.appspot.com",
+    messagingSenderId: "1027068821624",
+    appId: "1:1027068821624:web:9bca91a11f02ef7b852d8d",
+    measurementId: "G-33KCQDYLDE"
+  }
 };
 
 /*
